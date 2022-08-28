96452146bd7f9d0a4805bb33ccb55bda77c81697
Martina <Martina@DESKTOP-N04CUT1>
2022-08-28 04:36:21 -0300

    Agrego clase Rama

---
+++ src/main/java/com/mycompany/proyecto1/Clase_rama.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto1;
+
+/**
+ *
+ * @author Martina
+ */
+public class Clase_rama {
+    String nombre_rama;
+    public Clase_rama() {
+    }
+
+    public String getNombre_rama() {
+        return nombre_rama;
+    }
+
+    public void setNombre_rama(String nombre_rama) {
+        this.nombre_rama = nombre_rama;
+    }
+                                                                                                                                                                                        
+}
